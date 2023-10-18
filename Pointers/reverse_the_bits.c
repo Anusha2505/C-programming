960af85676f712f3e3f4a4369e51e11f937e6f41
Anusha <ssasa.anu@gmail.com>
2023-10-18 08:56:29 +0000

    converting an interger to bits

---
+++ Pointers/reverse_the_bits.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    
+    printf("enter any number\n");
+    scanf("%d",&a);
+    
+    char *p;
+    p = (char*)&a;
+    
+    for(int i =0; i<=7;i++){
+        if(*p & (1<<i)){
+            printf("1");
+        }
+        else{
+            printf("0");
+        }
+    }
+printf("\n");
+
+    return 0;
+}
