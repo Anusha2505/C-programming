5246d71029d1b432f6e47b9a7a2c2baa5a806743
Anusha <ssasa.anu@gmail.com>
2023-12-08 11:28:54 +0000

    printing only even numbers 1-100

---
+++ Functions/recursions/print_even_1-100.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void fun(int a);
+
+int main()
+{
+    int a =1;
+    fun(a);
+    
+    printf("\n");
+
+    return 0;
+}
+void fun(int a){
+    
+         a++;
+         
+        if(a<=100) {
+         
+        if(a%2 == 0)
+        {
+            printf("%d  ",a);
+            fun(a);
+        }
+        else{
+             fun(a);
+        }
+            
+        }
+   return;
+
+}
