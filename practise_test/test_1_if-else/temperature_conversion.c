19d160401d5531cc80684dd44b6613022a033ff5
Anusha <ssasa.anu@gmail.com>
2024-02-03 08:24:56 +0000

    converting the temperature from celsuis to farenheit and vice versa

---
+++ practise_test/test_1_if-else/temperature_conversion.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    float celsius,fahrenheit;
+    int num;
+    printf("Enter 1 for converting celsius to fahrenheit :\n");
+    printf("Enter 2 for converting fahrenheit to celsius : \n");
+    scanf("%d",&num);
+    switch(num){
+        case 1:
+            printf("Enter celsius :");
+            scanf("%f",&celsius);
+            fahrenheit = celsius* 9/5 +32;
+             printf("fahrenheit = %f\n",fahrenheit);
+             break;
+        case 2 :
+             printf("Enter fahrenheit :");
+            scanf("%f",&fahrenheit);
+            celsius = (fahrenheit - 32) * 5/9;
+            printf("celsius = %f\n",celsius);
+             break;
+             
+        default :
+            printf("Enter either 1 or 2\n");
+            break;
+    }
+   
+
+    return 0;
+}
